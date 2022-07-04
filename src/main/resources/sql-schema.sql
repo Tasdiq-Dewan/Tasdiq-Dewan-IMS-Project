0605a4eecf1c5aec9a501c641b0d13da90ba29bf
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-04 14:17:26 +0100

    added item table to sql schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,12 +1,21 @@
 drop schema ims;
 
-CREATE SCHEMA IF NOT EXISTS `ims`;
+CREATE SCHEMA IF NOT EXISTS ims;
 
-USE `ims` ;
+USE ims ;
 
-CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS customers (
+    `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) NOT NULL,
+    `surname` VARCHAR(40) NOT NULL,
+    PRIMARY KEY (`customer_id`)
 );
+
+CREATE TABLE IF NOT EXISTS items (
+	`item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) NOT NULL,
+    `value` DECIMAL(8, 2) NOT NULL,
+    PRIMARY KEY (`item_id`)
+);
+
+
