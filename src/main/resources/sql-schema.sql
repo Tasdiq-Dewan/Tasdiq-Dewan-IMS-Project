38b68c68c80d9025276857551572006342ec35c2
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-05 10:31:29 +0100

    changed customer_id to id to work with prewritten CustomerDAO

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS ims;
 USE ims ;
 
 CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT(11) NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(40) NOT NULL,
     surname VARCHAR(40) NOT NULL,
     PRIMARY KEY (customer_id)
