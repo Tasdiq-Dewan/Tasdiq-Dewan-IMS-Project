f64cffea09aa2dbbc345751452a42f6d65f3df7f
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-05 14:48:22 +0100

    added on delete cascade condition to foreign keys

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS orders(
 	order_id INT(11) NOT NULL AUTO_INCREMENT,
     customer_id INT(11) NOT NULL,
     PRIMARY KEY (order_id),
-    FOREIGN KEY (customer_id) REFERENCES customers(id)
+    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS itemorders(
@@ -30,8 +30,8 @@ CREATE TABLE IF NOT EXISTS itemorders(
     order_id INT(11) NOT NULL,
     item_id INT(11) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (item_id) REFERENCES items(item_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE
 );
 
 
