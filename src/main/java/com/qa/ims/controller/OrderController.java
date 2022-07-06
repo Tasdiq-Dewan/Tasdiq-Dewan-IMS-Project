1f3346fcde8359efc4d368c0910731139ef4c8cd
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-06 10:08:23 +0100

    created OrderController.java

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private OrderDAO orderDAO;
+	private Utils utils;
+
+	public OrderController(OrderDAO orderDAO, Utils utils) {
+		super();
+		this.orderDAO = orderDAO;
+		this.utils = utils;
+	}
+
+}
