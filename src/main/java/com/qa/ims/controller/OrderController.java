b785ad8ab87064002163df6faa3ce7e8e84b1270
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-06 11:36:37 +0100

    removed unused imports

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -5,11 +5,8 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.qa.ims.persistence.dao.CustomerDAO;
 import com.qa.ims.persistence.dao.OrderDAO;
 import com.qa.ims.persistence.domain.Order;
-import com.qa.ims.persistence.domain.Customer;
-import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.Utils;
 
 public class OrderController implements CrudController<Order> {
