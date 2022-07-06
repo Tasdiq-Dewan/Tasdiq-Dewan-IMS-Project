6250280c1603823059b483b521a34fb08af47ff5
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-06 09:39:28 +0100

    created ItemController.java

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,79 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController<Item> {
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+	private Utils utils;
+
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+
+	/**
+	 * Reads all customers to the logger
+	 */
+	@Override
+	public List<Item> readAll() {
+		List<Item> items = itemDAO.readAll();
+		for (Item item : items) {
+			LOGGER.info(item);
+		}
+		return items;
+	}
+
+	/**
+	 * Creates a customer by taking in user input
+	 */
+	@Override
+	public Customer create() {
+		LOGGER.info("Please enter a first name");
+		String firstName = utils.getString();
+		LOGGER.info("Please enter a surname");
+		String surname = utils.getString();
+		Customer customer = customerDAO.create(new Customer(firstName, surname));
+		LOGGER.info("Customer created");
+		return customer;
+	}
+
+	/**
+	 * Updates an existing customer by taking in user input
+	 */
+	@Override
+	public Customer update() {
+		LOGGER.info("Please enter the id of the customer you would like to update");
+		Long id = utils.getLong();
+		LOGGER.info("Please enter a first name");
+		String firstName = utils.getString();
+		LOGGER.info("Please enter a surname");
+		String surname = utils.getString();
+		Customer customer = customerDAO.update(new Customer(id, firstName, surname));
+		LOGGER.info("Customer Updated");
+		return customer;
+	}
+
+	/**
+	 * Deletes an existing customer by the id of the customer
+	 * 
+	 * @return
+	 */
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the customer you would like to delete");
+		Long id = utils.getLong();
+		return customerDAO.delete(id);
+	}
+}
