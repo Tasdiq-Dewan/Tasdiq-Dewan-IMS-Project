d176fdd80d64ee7acfac4a577175d9cfa704f392
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-07 12:43:51 +0100

    created testcreate and testreadall methods ItemControllerTest.java

--- src/test/java/com/qa/ims/controllers/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -1,6 +1,8 @@
 package com.qa.ims.controllers;
 
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +17,7 @@ import com.qa.ims.controller.CustomerController;
 import com.qa.ims.controller.ItemController;
 import com.qa.ims.persistence.dao.CustomerDAO;
 import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.Utils;
 
@@ -29,5 +32,34 @@ public class ItemControllerTest {
 
 	@InjectMocks
 	private ItemController controller;
+	
+	@Test
+	public void testCreate() {
+		final String name = "box"; final Double value = 5.00d;
+		final Item created = new Item(name, value);
+		Mockito.when(utils.getString()).thenReturn(name);
+		Mockito.when(utils.getDouble()).thenReturn(value);
+		Mockito.when(dao.create(created)).thenReturn(created);
+		
+		assertEquals(created, controller.create());
+		
+		Mockito.verify(utils, Mockito.times(1)).getString();
+		Mockito.verify(utils, Mockito.times(1)).getDouble();
+		Mockito.verify(dao, Mockito.times(1)).create(created);
+	}
+	
+	@Test
+	public void testReadAll() {
+		List<Item> items = new ArrayList<>();
+		items.add(new Item(1L, "book", 10.00d));
+		items.add(new Item(1L, "hate", 18.50d));
+		items.add(new Item(1L, "pen", 1.00d));
+
+		Mockito.when(dao.readAll()).thenReturn(items);
+
+		assertEquals(items, controller.readAll());
+
+		Mockito.verify(dao, Mockito.times(1)).readAll();
+	}
 
 }
