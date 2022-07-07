7b5b28f21a5577408bbe400f02c343395e95a1d7
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-07 09:35:59 +0100

    fixed test sql files so CustomerDAOTest runs

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,31 +1,34 @@
 
-
-CREATE TABLE IF NOT EXISTS customers (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(40) NOT NULL,
-    surname VARCHAR(40) NOT NULL,
-    PRIMARY KEY (id)
+DROP TABLE `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) NOT NULL,
+    `surname` VARCHAR(40) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS items (
-	item_id INT(11) NOT NULL AUTO_INCREMENT,
-    item_name VARCHAR(40) NOT NULL,
-    item_value DECIMAL(8, 2) NOT NULL,
-    PRIMARY KEY (item_id)
+DROP TABLE `items`;
+CREATE TABLE IF NOT EXISTS `items` (
+	`item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) NOT NULL,
+    `item_value` DECIMAL(8, 2) NOT NULL,
+    PRIMARY KEY (`item_id`)
 );
 
-CREATE TABLE IF NOT EXISTS orders(
-	order_id INT(11) NOT NULL AUTO_INCREMENT,
-    customer_id INT(11) NOT NULL,
-    PRIMARY KEY (order_id),
-    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
+DROP TABLE `orders`;
+CREATE TABLE IF NOT EXISTS `orders`(
+	`order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    PRIMARY KEY (`order_id),
+    FOREIGN KEY (`customer_id`) REFERENCES customers(`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS itemorders(
-	id INT(11) NOT NULL AUTO_INCREMENT, 
-    order_id INT(11) NOT NULL,
-    item_id INT(11) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE
+DROP TABLE `itemorders`;
+CREATE TABLE IF NOT EXISTS `itemorders`(
+	`id` INT(11) NOT NULL AUTO_INCREMENT, 
+    `order_id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`order_id`) REFERENCES `orders`(`order_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`item_id`) ON DELETE CASCADE
 );
