160967908ea4c1d6ded2c42bb5bd3a357fca3f8f
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-07 10:30:22 +0100

    created test methods for ItemDAOTest

--- src/test/java/com/qa/ims/persistence/dao/ItemDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/ItemDAOTest.java
@@ -20,5 +20,42 @@ public class ItemDAOTest {
 		DBUtils.connect();
 		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
 	}
-
+	
+	@Test
+	public void testCreate() {
+		final Item created = new Item(4L, "box", 5.00d);
+		assertEquals(created, DAO.create(created));
+	}
+	
+	@Test
+	public void testReadAll() {
+		List<Item> expected = new ArrayList<Item>();
+		expected.add(new Item(1L, "book", 10.00d));
+		expected.add(new Item(2L, "hat", 18.50d));
+		expected.add(new Item(3L, "pen", 1.00d));
+		assertEquals(expected, DAO.readAll());
+	}
+	
+	@Test
+	public void testReadLatest() {
+		Item expected = new Item(3L, "pen", 1.00d);
+		assertEquals(expected, DAO.readLatest());
+	}
+	
+	@Test
+	public void testRead() {
+		Item expected = new Item(1L, "book", 10.00d);
+		assertEquals(expected, DAO.read(1L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		Item expected = new Item(1L, "textbook", 50.00d);
+		assertEquals(expected, DAO.update(expected));
+	}
+	
+	@Test
+	public void testDelete() {
+		assertEquals(1, DAO.delete(1L));
+	}
 }
