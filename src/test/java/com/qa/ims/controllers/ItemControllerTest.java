289f3e2d9befc9154f69797c17e23d0c82063242
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-07 13:19:33 +0100

    created testupdate and testdelete methods ItemControllerTest.java

--- src/test/java/com/qa/ims/controllers/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controllers/ItemControllerTest.java
@@ -61,5 +61,35 @@ public class ItemControllerTest {
 
 		Mockito.verify(dao, Mockito.times(1)).readAll();
 	}
+	
+	@Test
+	public void testUpdate() {
+		Item updated = new Item(1L, "textbook", 50.00d);
+
+		Mockito.when(this.utils.getLong()).thenReturn(1L);
+		Mockito.when(this.utils.getString()).thenReturn(updated.getItemName());
+		Mockito.when(this.utils.getDouble()).thenReturn(updated.getValue());
+		Mockito.when(this.dao.update(updated)).thenReturn(updated);
+
+		assertEquals(updated, this.controller.update());
+
+		Mockito.verify(this.utils, Mockito.times(1)).getLong();
+		Mockito.verify(this.utils, Mockito.times(1)).getString();
+		Mockito.verify(this.utils, Mockito.times(1)).getDouble();
+		Mockito.verify(this.dao, Mockito.times(1)).update(updated);
+	}
+	
+	@Test
+	public void testDelete() {
+		final long ID = 1L;
+
+		Mockito.when(utils.getLong()).thenReturn(ID);
+		Mockito.when(dao.delete(ID)).thenReturn(1);
+
+		assertEquals(1L, this.controller.delete());
+
+		Mockito.verify(utils, Mockito.times(1)).getLong();
+		Mockito.verify(dao, Mockito.times(1)).delete(ID);
+	}
 
 }
